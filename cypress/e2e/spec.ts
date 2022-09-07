f6dbb8688f781a2dd2ce368c6dd8f63ec1908712
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2022-09-08 03:56:22 +0530

    Update spec.ts

--- cypress/e2e/spec.ts
+++ cypress/e2e/spec.ts
@@ -2,13 +2,18 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('Phatak Status')
-    if (cy.contains('Phatak Lists')) {
-      cy.get('button').contains('Show Form').click()
-    }
+    // if (cy.contains('Phatak Lists').) {
+    //   cy.get('button').contains('Show Form').click()
+    // }
     if (cy.contains('Add Phatak')) {
-      cy.get('button').contains('Submit').click()
+      cy.get('input').contains('Submit').click()
       cy.focused().blur()
       cy.contains('Phatak Name is Required')
+      cy.scrollTo('top')
+      cy.get('input[formcontrolname="phatakName"]').type('Rajiv Chowk Railway Crossing',{force: true})
+      cy.get('input').contains('Submit').click()
+      cy.focused().blur()
+      cy.contains('Phone Number is Required')
     }
   })
 })
