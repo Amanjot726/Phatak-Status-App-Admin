cbb3a669da926f4f7064828d05d2b32fb3c7892f
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2022-09-13 21:59:49 +0530

    Updated QA Testing file

--- cypress/e2e/spec.ts
+++ cypress/e2e/spec.ts
@@ -1,17 +1,18 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
+    cy.wait(1000)
     cy.contains('Phatak Status')
     // if (cy.contains('Phatak Lists').) {
     //   cy.get('button').contains('Show Form').click()
     // }
     if (cy.contains('Add Phatak')) {
-      cy.get('input').contains('Submit').click()
+      cy.get('button').contains('Submit').click()
       cy.focused().blur()
       cy.contains('Phatak Name is Required')
       cy.scrollTo('top')
       cy.get('input[formcontrolname="phatakName"]').type('Rajiv Chowk Railway Crossing',{force: true})
-      cy.get('input').contains('Submit').click()
+      cy.get('button').contains('Submit').click()
       cy.focused().blur()
       cy.contains('Phone Number is Required')
     }
